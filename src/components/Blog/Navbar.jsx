16bdfd3dab3c7c007116fe15f18315cc6f2d3d51
caamillo <mcamillolud@gmail.com>
2023-10-23 17:36:56 +0200

    test

--- src/components/Blog/Navbar.jsx
+++ src/components/Blog/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Navbar() {
     return (
         <div className="fixed top-0 left-0 w-full z-[999999] h-[var(--blog-nav-size)] flex justify-start md:justify-center items-center bg-[#353e4ed1] backdrop-blur-lg">
-            <Link href={ '/blog' } className="text-white font-bold text-4xl mx-5">FemboyBlog</Link>
+            <Link href={ '/blog' } className="text-white font-bold text-4xl mx-5">FemboyBlog2</Link>
         </div>
     )
 }
