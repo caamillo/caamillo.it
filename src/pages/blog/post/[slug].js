7a87987fb652be2d0e552d261cce00e35447403c
caamillo <mcamillolud@gmail.com>
2023-11-09 10:42:37 +0100

    nsfw modal

--- src/pages/blog/post/[slug].js
+++ src/pages/blog/post/[slug].js
@@ -1,17 +1,40 @@
 // React
+import { useEffect, useRef } from 'react';
 import Link from 'next/link'
 
 // Deps
 import { MdPreview, MdCatalog } from 'md-editor-rt';
 import 'md-editor-rt/lib/preview.css';
 import prisma from '@/lib/prisma'
+import { useLocalStorage } from 'usehooks-ts';
 
 // Components
 import BlogLayout from '@/components/Blog/BlogLayout'
 
 export default function Post({ post }) {
+
+    const [ nsfw, setNsfw ] = useLocalStorage('nsfw', false)
+    const nsfwModalRef = useRef()
+
+    useEffect(() => {
+        if (!nsfw && post.tags.filter(tag => tag.toLowerCase() === 'nsfw').length) setTimeout(() => nsfwModalRef.current?.setAttribute('data-show', 'true'), 5e2)
+        else nsfwModalRef.current?.setAttribute('data-show', 'false')
+    }, [ nsfw ])
+
     return (
         <BlogLayout>
+            <div ref={ nsfwModalRef } data-show="false" className="nsfw-warning w-screen h-screen fixed top-0 left-0 duration-500 z-[999999] transition-all">
+                <div className='absolute top-0 left-1/2 -translate-x-1/2 container flex justify-center'>
+                    <div className='warning-modal transition-transform duration-1000 ease-in-out bg-white w-full lg:w-2/3 xl:w-1/2 p-5 rounded-lg mt-10 mx-3 border-2 border-slate-300 flex flex-col items-center text-center'>
+                        <h2 className='font-bold text-3xl'>Warning</h2>
+                        <p className='text-lg md:text-xl mt-1'>This post may includes some <i className=' text-slate-600 font-semibold'>#NSFW</i> content.<br/>Are you sure to proceed?</p>
+                        <div className='grid md:grid-cols-2 w-full mt-5 gap-3 md:gap-5'>
+                            <Link href='/blog' className='outline-2 outline-slate-300 text-slate-700 outline rounded-lg py-4 order-1 font-semibold'>Go Back</Link>
+                            <button onClick={ () => setNsfw(true) } className='bg-slate-800 text-white rounded-lg py-4 md:order-2 font-semibold'>Sure.</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className='bg-white p-5 rounded-lg shadow-sm'>
                 <h1 className='text-4xl font-bold'>{ post.title }</h1>
                 <h2 className='text-xl font-medium'>{ post.description }</h2>
