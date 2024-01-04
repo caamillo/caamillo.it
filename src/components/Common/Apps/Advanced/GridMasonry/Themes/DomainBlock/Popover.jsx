64d5b72cc72b49ee2e28a8ca580222a36ca29781
caamillo <mcamillolud@gmail.com>
2024-01-04 17:21:48 +0100

    popover shows on more click

--- src/components/Common/Apps/Advanced/GridMasonry/Themes/DomainBlock/Popover.jsx
+++ src/components/Common/Apps/Advanced/GridMasonry/Themes/DomainBlock/Popover.jsx
@@ -1,25 +1,70 @@
-export default function Popover({ name="", options=[] }) {
+import { useEffect, useState, useRef } from "react"
+
+export default function Popover({ isOpen=false, setIsOpen, name="", offset={ x: 0 }, options=[] }) {
+
+    const [ left, setLeft ] = useState(true)
+    const popoverRef = useRef()
+    const SELECTION_OFFSET = 10
+
+    const onScreenChange = () => {
+        const { x } = popoverRef.current.getBoundingClientRect()
+        if (x >= window.innerWidth / 2) setLeft(false)
+        else setLeft(true)
+    }
+
+    useEffect(() => {
+        const onBetrayal = (e) => {
+            if (!isOpen) return
+            const [ x, y ] = [ e.clientX, e.clientY ]
+            const rect = popoverRef.current.getBoundingClientRect()
+            const isMouseOut = x < rect.x - SELECTION_OFFSET || x > rect.right + SELECTION_OFFSET ||
+                y < rect.top - SELECTION_OFFSET || y > rect.bottom + SELECTION_OFFSET
+            // console.log(x, y, 'rect <->', rect.x - SELECTION_OFFSET, rect.right + SELECTION_OFFSET, 'rect |', rect.top - SELECTION_OFFSET, rect.bottom + SELECTION_OFFSET, 'condizione', isMouseOut)
+            if (isMouseOut) setIsOpen(false)
+        }
+
+        window.addEventListener('resize', onScreenChange)
+        window.addEventListener('click', onBetrayal)
+
+        return () => {
+            document.removeEventListener('resize', onScreenChange)
+            document.removeEventListener('click', onBetrayal)
+        }
+    })
+
+    useEffect(() => {
+        onScreenChange()
+    }, [])
+
     return (
-        <div className="w-fit bg-slate-100 p-2 pt-0 shadow-lg rounded-md rounded-tl-none min-w-[180px] max-w-[200px] overflow-hidden select-none flex flex-col justify-center items-center">
+        <div style={{
+                opacity: isOpen ? 1 : 0,
+                pointerEvents: isOpen ? 'all' : 'none',
+                left: left ? offset.x : 'auto',
+                right: left ? 'auto' : offset.x,
+                borderTopLeftRadius: left ? 0 : '.375rem',
+                borderTopRightRadius: left ? '.375rem' : 0,
+                top: offset?.y ?? 0,
+            }} ref={ popoverRef } className={`w-fit bg-slate-100 transition-all absolute cursor-default p-2 pt-0 shadow-lg rounded-md min-w-[180px] max-w-[200px] overflow-hidden select-none flex flex-col justify-center items-center ${ isOpen ? 'popover-open' : '' }`}>
             <div className="w-full px-3 py-2 h-full flex justify-between items-center space-x-2">
                 <p className="text-lg text-slate-500 font-bold">{ name }</p>
                 <img src="/icons/Apps/info.svg" className=" w-5" />
             </div>
             <div className="w-full rounded-md px-2 py-2 bg-white">
                 {
-                    options.map(option =>
+                    options.map((option, idx) =>
                         !option.hide && option.type === 0 ?
-                            <div className="w-full p-1 px-2">
+                            <div key={ `pop-opt-${ idx }` } className="w-full p-1 px-2">
                                 <p className="font-extrabold text-slate-700 uppercase text-xs tracking-wide">{ option.title }</p>
                                 {
                                     option.isSubHtml ? 
                                         option.subtitle :
                                         option.isDate ?
                                         <span className="text-slate-500 text-sm font-medium break-all">{ option.subtitle }</span> :
-                                        <span className="text-slate-500 font-medium break-all">{ option.subtitle.length > 30 ? option.subtitle.slice(0, 35) + '...' : option.subtitle }</span>
+                                        <span className="text-slate-500 font-medium break-all lowercase">{ option.subtitle.length > 30 ? option.subtitle.slice(0, 35) + '...' : option.subtitle }</span>
                                 }
                             </div> : !option.hide ?
-                            <div onClick={ () => option.action(options) } className="w-full mt-2 transition-colors group rounded-md p-1 px-2 cursor-pointer hover:bg-slate-200/70 flex space-x-3 justify-start items-center">
+                            <div key={ `pop-opt-${ idx }` } onClick={ () => option.action(options) } className="w-full mt-2 transition-colors group rounded-md p-1 px-2 cursor-pointer hover:bg-slate-200/70 flex space-x-3 justify-start items-center">
                                 <img src={`/icons/Apps/${ option.icon }.svg`} className="w-4" />
                                 <p className="font-medium text-slate-500 group-hover:text-slate-600">{ option.name }</p>
                             </div> : ''
--- src/components/Common/Apps/Advanced/GridMasonry/Themes/DomainBlock/index.jsx
+++ src/components/Common/Apps/Advanced/GridMasonry/Themes/DomainBlock/index.jsx
@@ -4,12 +4,14 @@ import Image from "next/image"
 
 // Components
 import Placeholder from "@/components/Common/SkeletonLoader/Placeholder"
+import Popover from "./Popover"
 
 const INTERVAL_WAIT = 100
 
 export default function DomainBlock({ loaded, addLoaded, element, idx }) {
 
     const [ isLoading, setIsLoading ] = useState(true)
+    const [ isPopoverOpen, setIsPopoverOpen ] = useState(false)
     const imgRef = useRef()
     const placeHolderRef = useRef()
     const wrapperRef = useRef()
@@ -32,12 +34,14 @@ export default function DomainBlock({ loaded, addLoaded, element, idx }) {
         placeHolderRef.current.src = element.img.small
         imgRef.current.src = element.img.regular
     }, [])
+
+    useEffect(() => {console.log(`popover-${ idx }`, isPopoverOpen)}, [isPopoverOpen])
     
     return (
-        <div ref={ wrapperRef } style={{ opacity: 0 }} className="transition-opacity shadow-md duration-1000 w-full h-full rounded-md relative overflow-hidden bg-slate-200">
-            <div className="w-full h-full absolute bottom-0 left-0 bg-gradient-to-tr from-[#000000] z-30"></div>
-            <img ref={ placeHolderRef } style={{ opacity: 0 }} loading="lazy" className="duration-1000 transition-opacity absolute bottom-0 left-0 w-full h-full object-cover z-10 blur-md" />
-            <img ref={ imgRef } style={{ opacity: 0 }} loading="lazy" className="transition-opacity duration-1000 absolute bottom-0 left-0 w-full h-full object-cover z-20" />
+        <div ref={ wrapperRef } style={{ opacity: 0 }} className="transition-opacity shadow-md duration-1000 w-full h-full rounded-md relative bg-slate-200">
+            <div className="w-full h-full absolute rounded-md bottom-0 left-0 bg-gradient-to-tr from-[#000000] z-30"></div>
+            <img ref={ placeHolderRef } style={{ opacity: 0 }} loading="lazy" className="duration-1000 rounded-md transition-opacity absolute bottom-0 left-0 w-full h-full object-cover z-10 blur-md" />
+            <img ref={ imgRef } style={{ opacity: 0 }} loading="lazy" className="transition-opacity rounded-md duration-1000 absolute bottom-0 left-0 w-full h-full object-cover z-20" />
             <div className="text-white relative z-40 w-full h-full flex flex-col justify-end">
                 <div className="m-5 space-y-3">
                     <Placeholder loaded={ loaded }>
@@ -58,8 +62,67 @@ export default function DomainBlock({ loaded, addLoaded, element, idx }) {
                     </div>
                 </div>
             </div>
-            <div className="absolute w-[30px] h-[30px] bg-white top-0 right-0 z-50 m-3 rounded-full flex justify-center items-center cursor-pointer">
+            <div onClick={ () => setIsPopoverOpen(true) } className="absolute w-[30px] h-[30px] bg-white top-0 right-0 z-50 m-3 rounded-full flex justify-center items-center cursor-pointer">
                 <img src="/icons/Apps/more.svg" className="select-none" />
+                <Popover
+                    key={ `pop-${idx}` }
+                    name={ '.' + element.data?.tld }
+                    isOpen={ isPopoverOpen }
+                    setIsOpen={ setIsPopoverOpen }
+                    offset={{ x: 10, y: 10 }}
+                    left={ true }
+                    options={[
+                        {
+                            title: 'Domain Name',
+                            subtitle: 'tesTafss.com',
+                            type: 0
+                        },
+                        {
+                            title: 'Available',
+                            subtitle: <img src="/icons/Apps/thumb-up.svg" className="w-5 mt-1" />,
+                            isSubHtml: true,
+                            type: 0
+                        },
+                        {
+                            title: 'Registrar',
+                            subtitle: 'Testosterone Inc.',
+                            hide: true,
+                            type: 0
+                        },
+                        {
+                            title: 'Updated Date',
+                            subtitle: '01-01-2024',
+                            isDate: true,
+                            hide: true,
+                            type: 0
+                        },
+                        {
+                            title: 'Creation Date',
+                            subtitle: '01-01-2024',
+                            isDate: true,
+                            type: 0
+                        },
+                        {
+                            title: 'Expiration Date',
+                            subtitle: '01-01-2024',
+                            isDate: true,
+                            hide: true,
+                            type: 0
+                        },
+                        {
+                            title: 'Status',
+                            subtitle: 'Ok!',
+                            hide: true,
+                            type: 0
+                        },
+                        {
+                            name: 'Expand',
+                            icon: 'expand',
+                            type: 1,
+                            action: () => undefined
+                        }
+                    ]}
+                />
             </div>
         </div>
     )
